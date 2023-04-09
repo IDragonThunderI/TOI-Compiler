870069166badac26ebcf50ee44f56db1e0ae6759
IDragonThunderI <andrey.voltr@yandex.ru>
2023-04-09 04:39:12 +0400

    Добавьте файлы проекта.

---
+++ Compiler/Compiler.cpp
@@ -0,0 +1,13 @@
+﻿#include "LexAnalyser.h"
+#include "SyntaxAnalyser.h"
+#include "CodeGen.h"
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+	lexAnalysis(argv[1]);
+	syntaxAnalysis();
+	genTriads();
+	std::cout << "Компиляция успешно завершена!" << std::endl;
+	return 0;
+}
